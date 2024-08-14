18e636b02116a045dbe5c0bbea8222029f8d14a8
leetdvn <111244237+leetdvn@users.noreply.github.com>
2024-08-14 17:32:11 +0700

    Update BlenderExport.py

--- Scripts/BlenderExport.py
+++ Scripts/BlenderExport.py
@@ -1,2 +1,14 @@
 import bpy
-bpy.ops.export_scene.fbx(filepath='%1')
+
+#Export Cmd ALL
+##bpy.ops.export_scene.fbx(filepath='%1')
+#-----------Export Selection
+##bpy.ops.export_scene.fbx(filepath='C:/Users/leepl/OneDrive/Desktop/Present_Export/Taoday.fbx',use_selection=True)
+
+#Selection Object
+##obj = bpy.context.scene.objects['20B']
+##bpy.data.objects[obj.name].select_set(True); 
+
+#Create Cube
+# bpy.ops.mesh.primitive_cube_add(size=4,name="taoday")
+# cube_obj = bpy.context.active_object
