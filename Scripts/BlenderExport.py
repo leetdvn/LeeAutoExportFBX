caa41f6fa75148a1667854ee9d2bc1cf69ac4b25
leetdvn <111244237+leetdvn@users.noreply.github.com>
2024-08-23 00:55:40 +0700

    update Blender Script

--- Scripts/BlenderExport.py
+++ Scripts/BlenderExport.py
@@ -11,10 +11,35 @@ import bpy
 #Create Cube
 # bpy.ops.mesh.primitive_cube_add(size=4,name="taoday")
 # cube_obj = bpy.context.active_object
+def GetSelections():
+    return bpy.context.selected_objects
+
+def ClearSelection(): return bpy.ops.object.select_all(action='DESELECT')
+
+def isObjectType(inObject): return type(inObject) == bpy.types.Object
+
+def isCollection(inObject): return type(inObject) == bpy.types.Collection
+
+def GetObjectsInCollection(collection):
+    if collection is None: return []
+    return collection.all_objects
+
+def SelectAllObjsInCollection(inCollection):
+    if inCollection is None: return
+
+    ClearSelection()
+
+    for obj in GetObjectsInCollection(inCollection):
+        try:
+            bpy.context.scene.objects[obj.name]
+            bpy.data.objects[obj.name].select_set(True)
+        except:
+            pass
+
+####################MASSEXPORT FUNC###########################################
 def LeeMassExport():
     Export = "MassExport"
     col = bpy.data.collections[Export]
-    print(col)
     for o in col.objects:
         bpy.ops.object.select_all(action='DESELECT')
         bpy.context.scene.objects[o.name]
@@ -23,6 +48,30 @@ def LeeMassExport():
         fbx=bpy.ops.export_scene.fbx(filepath=expPath,use_selection=True)
         print("Exported  : " + expPath + "\n")
 
+    ChildCollections  = [c for c in col.children if isCollection(c)]
+    if ChildCollections.__len__() <= 0: return
+
+    for child in ChildCollections:
+        SelectAllObjsInCollection(child)
+        expPath = '%1'  + child.name + ".fbx"
+        fbx=bpy.ops.export_scene.fbx(filepath=expPath,use_selection=True)
+        print("Exported  : " + expPath + "\n")
+    #print(ChildCollections)
+
 LeeMassExport()
-#Bake Layer Clear Contrains
-#bpy.ops.nla.bake(frame_start=112, frame_end=225, visual_keying=True, clear_constraints=True, clear_parents=True, use_current_action=True, clean_curves=True, bake_types={'POSE'})
+
+# Export = "MassExport"
+# col = bpy.data.collections[Export]
+# #for o in col.children:
+#     #print(o.name)
+#     #print(o.objects)
+#     #bpy.data.objects[o.name].select_set(True)
+#     #for x in o.children:
+#         #print(x.objects.data)
+# chopchop = col.children[0].objects.data
+# chopchop = bpy.data.collections[chopchop.name]
+
+# for o in chopchop.children:
+#     print(type(o))
+#print(chopchop)
+#bpy.ops.outliner.item_activate(deselect_all=True)
